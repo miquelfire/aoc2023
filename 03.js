cf1e51e533bb6115ae3e6dbe01f8ccbd767d8a9d
Miquel Fire Burns <miquelfire@gmail.com>
2023-12-03 13:48:06 -0500

    Day 3 Part 2 Done

--- 03.js
+++ 03.js
@@ -17,7 +17,6 @@ export const part1 = async d => {
 				};
 			});
 
-		// Skip lines without a part number
 		partLabels.forEach((partLabel) => {
 			let foundPart = false;
 			for (let y = Math.max(0, row - 1); y <= Math.min(data.length - 1, row + 1); y++) {
@@ -41,6 +40,40 @@ export const part1 = async d => {
  */
 export const part2 = async d => {
 	const data = d.split('\n');
-	data.length = 0;
-	return data;
+	const gearSearch = /\*/g;
+	const numberSearch = /\d/;
+	const partNumberSearch = /\d+/g;
+	let sum = 0;
+
+	data.forEach((rowData, row) => {
+		const gears = [...rowData.matchAll(gearSearch)]
+			.map(e => {
+				return {
+					colStart: Math.max(0, e.index - 1),
+					colEnd: Math.min(rowData.length - 1, e.index + e[0].length),
+				};
+			});
+
+		gears.forEach(gear => {
+			const partNumbersFound = [];
+			for (let y = Math.max(0, row - 1); y <= Math.min(data.length - 1, row + 1); y++) {
+				const partNumbers = [...data[y].matchAll(partNumberSearch)];
+				partNumbers.forEach(e => {
+					if (e.index <= gear.colEnd && (e.index + e[0].length - 1) >= gear.colStart) {
+						partNumbersFound.push(parseInt(e[0], 10));
+					}
+				});
+			}
+			if (partNumbersFound.length == 2) {
+				sum += partNumbersFound.reduce((p, v) => p * v, 1);
+			}
+			if (partNumbersFound.length > 2) {
+				console.log(partNumbersFound);
+				console.log(gear);
+				debugger;
+			}
+		});
+	});
+
+	return sum;
 };
