3f298ce287384155cc512e241a2743f88730483a
Miquel Fire Burns <miquelfire@gmail.com>
2023-12-04 00:57:08 -0500

    Day 4 Part 2 done

--- 04.js
+++ 04.js
@@ -22,7 +22,21 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
+	const cardMatch = /Card +\d+: +([ \d]+) \| (.*)/;
+	const data = d.split('\n')
+		.map(card => {
+			const matchedNumbers = [];
+			const [,winningNumbers, yournumbers] = card.match(cardMatch).map(e => e.split(/ +/g));
+			matchedNumbers.push(...winningNumbers.filter(number => yournumbers.includes(number)));
+			return [1, matchedNumbers.length];
+		})
+		.map((card, index, data) => {
+			for (let i = index + 1; i <= index + card[1]; i++) {
+				data[i][0] += card[0];
+			}
+			return (card[0]);
+		})
+		.reduce((p, v) => p + v, 0);
+
 	return data;
 };
