daae8d4238b6f388d5c22982068967f0bd790e43
Miquel Fire Burns <miquelfire@gmail.com>
2023-12-06 16:42:33 -0500

    Day 6 part 2 done

--- 06.js
+++ 06.js
@@ -3,11 +3,11 @@
  */
 export const part1 = async d => {
 	const data = d.split('\n').map(e => e.split(/ +/).slice(1).map(e => parseInt(e, 10)));
-	const records = [{time: 0, dist: 0}];
+	const records = [{ time: 0, dist: 0 }];
 	let winsTotal = 1;
 	records.pop(); // Remove the demo record put to allow autocomplete to work
 	data[0].forEach((e, i) => {
-		records.push({time: e, dist: data[1][i]});
+		records.push({ time: e, dist: data[1][i] });
 	});
 	records.forEach(record => {
 		let wins = 0;
@@ -26,7 +26,13 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n').map(e => parseInt(e.replace(/\D+/g, ''), 10));
+	let wins = 0;
+	for (let i = 1; i < data[0]; i++) {
+		const dist = i * (data[0] - i);
+		if (dist > data[1]) {
+			wins++;
+		}
+	}
+	return wins;
 };
