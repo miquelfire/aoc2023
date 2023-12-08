9b73c78d63ab5979679d85981d22b714faa976ea
Miquel Fire Burns <miquelfire@gmail.com>
2023-12-08 09:46:49 -0500

    Day 8 Part 1 Done

---
+++ 08.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n\n');
+	const directions = data[0].split('');
+	const nodes = new Map();
+	data[1].split('\n').map(e => {
+		const parts = e.match(/([a-zA-Z]+) = \(([a-zA-Z]+), ([a-zA-Z]+)\)/);
+		if (parts) {
+			nodes.set(parts[1], [parts[2], parts[3]]);
+		}
+	});
+	let currentNode = 'AAA';
+	let steps = 0;
+	while (currentNode !== 'ZZZ') {
+		const dir = nodes.get(currentNode);
+		const dirPos = steps % directions.length;
+		switch (directions[dirPos]) {
+			case 'L':
+				currentNode = dir[0];
+				break;
+			case 'R':
+				currentNode = dir[1];
+				break;
+			default:
+				console.log('Ah Fuck!');
+				debugger;
+				break;
+		}
+		steps++;
+	}
+	return steps;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n\n');
+	data.splice(0, data.length);
+	return data;
+};
