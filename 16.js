002ef003adb4b063b05514f1b5e90a5102533db8
Miquel Fire Burns <miquelfire@gmail.com>
2023-12-16 13:22:55 -0500

    Day 16 Part 1 Done

---
+++ 16.js
@@ -0,0 +1,207 @@
+const Dir = Object.freeze({
+	North: Symbol('north'),
+	South: Symbol('south'),
+	West: Symbol('west'),
+	East: Symbol('east'),
+});
+
+class Beam {
+	/**
+	 * @param {number} y 
+	 * @param {number} x 
+	 * @param {Dir} dir 
+	 */
+	constructor(y, x, dir) {
+		this.#x = x;
+		this.#y = y;
+		this.#dir = dir;
+	}
+
+	get yx() {
+		return this.#y + 'x' + this.#x;
+	}
+
+	get yxd() {
+		const ret = this.yx + 'x';
+		switch (this.#dir) {
+			case Dir.North:
+				return ret + 'north';
+			case Dir.South:
+				return ret + 'south';
+			case Dir.West:
+				return ret + 'west';
+			case Dir.East:
+				return ret + 'east';
+		}
+		return this.yx + 'x' + this.#dir.toString();
+	}
+
+	/**
+	 * @param {string[][]} data 
+	 * @returns {Beam[]}
+	 */
+	moveBeam(data) {
+		const ret = [];
+		switch (this.#dir) {
+			case Dir.North:
+				if (this.#y > -1) { // While not needed, just in case
+					switch (data[this.#y][this.#x]) {
+						case '-':
+							if (this.#x - 1 > -1) {
+								ret.push(new Beam(this.#y, this.#x - 1, Dir.West));
+							}
+							if (this.#x + 1 < data[this.#y].length) {
+								ret.push(new Beam(this.#y, this.#x + 1, Dir.East));
+							}
+							break;
+						case '/':
+							if (this.#x + 1 < data[this.#y].length) {
+								ret.push(new Beam(this.#y, this.#x + 1, Dir.East));
+							}
+							break;
+						case '\\':
+							if (this.#x - 1 > -1) {
+								ret.push(new Beam(this.#y, this.#x - 1, Dir.West));
+							}
+							break;
+						default:
+							if (this.#y - 1 > -1) {
+								ret.push(new Beam(this.#y - 1, this.#x, this.#dir));
+							}
+					}
+				}
+				break;
+			case Dir.South:
+				if (this.#y < data.length) {
+					switch (data[this.#y][this.#x]) {
+						case '-':
+							if (this.#x - 1 > -1) {
+								ret.push(new Beam(this.#y, this.#x - 1, Dir.West));
+							}
+							if (this.#x + 1 < data[this.#y].length) {
+								ret.push(new Beam(this.#y, this.#x + 1, Dir.East));
+							}
+							break;
+						case '/':
+							if (this.#x - 1 > -1) {
+								ret.push(new Beam(this.#y, this.#x - 1, Dir.West));
+							}
+							break;
+						case '\\':
+							if (this.#x + 1 < data[this.#y].length) {
+								ret.push(new Beam(this.#y, this.#x + 1, Dir.East));
+							}
+							break;
+						default:
+							if (this.#y + 1 < data.length) {
+								ret.push(new Beam(this.#y + 1, this.#x, this.#dir));
+							}
+							break;
+					}
+				}
+				break;
+			case Dir.West:
+				if (this.#x > -1) {
+					switch (data[this.#y][this.#x]) {
+						case '|':
+							if (this.#y - 1 > -1) {
+								ret.push(new Beam(this.#y - 1, this.#x, Dir.North));
+							}
+							if (this.#y + 1 < data.length) {
+								ret.push(new Beam(this.#y + 1, this.#x, Dir.South));
+							}
+							break;
+						case '/':
+							if (this.#y + 1 < data.length) {
+								ret.push(new Beam(this.#y + 1, this.#x, Dir.South));
+							}
+							break;
+						case '\\':
+							if (this.#y - 1 > -1) {
+								ret.push(new Beam(this.#y - 1, this.#x, Dir.North));
+							}
+							break;
+						default:
+							if (this.#x - 1 > -1) {
+								ret.push(new Beam(this.#y, this.#x - 1, this.#dir));
+							}
+							break;
+					}
+				}
+				break;
+			case Dir.East:
+				if (this.#x < data[this.#y].length) {
+					switch (data[this.#y][this.#x]) {
+						case '|':
+							if (this.#y - 1 > -1) {
+								ret.push(new Beam(this.#y - 1, this.#x, Dir.North));
+							}
+							if (this.#y + 1 < data.length) {
+								ret.push(new Beam(this.#y + 1, this.#x, Dir.South));
+							}
+							break;
+						case '/':
+							if (this.#y - 1 > -1) {
+								ret.push(new Beam(this.#y - 1, this.#x, Dir.North));
+							}
+							break;
+						case '\\':
+							if (this.#y + 1 < data.length) {
+								ret.push(new Beam(this.#y + 1, this.#x, Dir.South));
+							}
+							break;
+						default:
+							if (this.#x + 1 < data[this.#y].length) {
+								ret.push(new Beam(this.#y, this.#x + 1, this.#dir));
+							}
+							break;
+					}
+				}
+				break;
+		}
+
+		return ret;
+	}
+
+	#x;
+	#y;
+	#dir;
+}
+
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const beams = [new Beam(0, 0, Dir.East)];
+	const data = d.split('\n').map(e => e.split(''));
+	const lit = new Set(['0x0']);
+	const visited = new Set();
+	while (beams.length) {
+		/** @type {Beam[]} */
+		const queue = [];
+		while (beams.length) {
+			const beam = beams.shift();
+			lit.add(beam.yx);
+			if (!visited.has(beam.yxd)) {
+				queue.push(beam);
+			}
+			visited.add(beam.yxd);
+		}
+
+		while (queue.length) {
+			const beam = queue.pop();
+			const newBeams = [...beam.moveBeam(data)];
+			beams.push(...newBeams);
+		}
+	}
+	return lit.size;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n');
+	data.splice(0, data.length);
+	return data;
+};
